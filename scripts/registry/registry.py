9a2be8d7db5d34ba12779397818a3a9b8b820530
cotsom <gerfy.17@mail.ru>
2022-12-09 21:41:10 +0300

    update

--- scripts/registry/registry.py
+++ scripts/registry/registry.py
@@ -1,3 +1,5 @@
+import random
+import string
 import subprocess
 from bs4 import BeautifulSoup
 import requests
@@ -6,7 +8,27 @@ from potee import ServiceBase
 port = 5000
 srv = ServiceBase()
 
+manifest = {
+   "schemaVersion": 2,
+   "mediaType": "application/vnd.docker.distribution.manifest.v2+json",
+   "config": {
+      "mediaType": "application/vnd.docker.container.image.v1+json",
+      "size": 1472,
+      "digest": "sha256:49176f190c7e9cdb51ac85ab6c6d5e4512352218190cd69b08e6fd803ffbf3da"
+   },
+   "layers": [
+      {
+         "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
+         "size": 3370706,
+         "digest": "sha256:c158987b05517b6f2c5913f3acef1f2182a32345a304fe357e3ace5fadcad715"
+      }
+   ]
+}
 
+def gen_tag(length=10):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
 
 @srv.ping
 def ping(host):
@@ -16,18 +38,19 @@ def ping(host):
 
 @srv.put("image")
 def put_image(host, flag):
-    tag_image = f'tag alpine {host}:{port}/{flag}'
-    push_image = f'push {host}:{port}/{flag}'
-    subprocess.call(["docker", "tag", "alpine", f'{host}:{port}/{flag}'], stdout=subprocess.DEVNULL)
-    subprocess.call(["docker", "push", f'{host}:{port}/{flag}'], stdout=subprocess.DEVNULL)
-    return flag
+    _id = gen_tag()
+    manifest['flag'] = flag
+    headers = {'Content-Type': 'application/vnd.docker.distribution.manifest.v2+json'}
+    r = requests.put(f'http://{host}:{port}/v2/alpine/manifests/{_id}', headers=headers, json=manifest)
+    #TODO: answer check status
+    #if r.status_code
+    return _id
 
 @srv.get("image")
-def get_image(host, id):
-    r = requests.get(f"http://{host}:{port}/v2/_catalog")
-    if f"\"{id}\"" in r.text:
-        return 1
-    return 0
+def get_image(host, _id):
+    headers = {'Accept': 'application/vnd.docker.distribution.manifest.v2+json'}
+    r = requests.get(f"http://{host}:{port}/v2/alpine/manifests/{_id}", headers=headers).json()
+    return r.get("flag")
     
 @srv.exploit("image")
 def get_image(host):
